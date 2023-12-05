d813d513ff42f0441a86829f542d13c3d229a059
Liquid <Liquid369@gmail.com>
2023-12-05 14:59:37 -0600

    Add other files to build

--- build.rs
+++ build.rs
@@ -1,6 +1,12 @@
 fn main() {
     cc::Build::new()
         .file("src/quark/quark.c") // or .cpp
+        .file("src/quark/blake.c")
+        .file("src/quark/bmw.c")
+        .file("src/quark/groestl.c")
+        .file("src/quark/jh.c")
+        .file("src/quark/keccak.c")
+        .file("src/quark/skein.c")
         .include("src/quark") // include directory for headers
-        .compile("quark");
+        .compile("libquark.a");
 }
